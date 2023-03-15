dea55354e3d974ed55cd106c500b2fea5c106e33
Moisés Reis <118673344+moisesmreis@users.noreply.github.com>
2023-03-14 21:03:54 -0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,20 +9,18 @@ export default function Header(props) {
     const router = useRouter();
 
     return (
-        <header className="grid grid-cols-2 bg-white my-12 py-2 gap-4">
+        <header className="grid grid-cols-1 lg:grid-cols-2 bg-white my-12 py-2 gap-4">
             <div className="flex justify-start content-center items-center flex-row">
                 <Link href="/">
                     <Logo />
                 </Link>
             </div>
-            <div className="flex justify-end content-center items-center flex-row gap-6">
+            <div className="flex justify-start lg:justify-end content-center items-center flex-row gap-6">
                 <Link href="/">
                     <ButtonText className={router.pathname == "/" ? "opacity-100" : ""}>Início</ButtonText>
                 </Link>
-                <ButtonText>Sobre</ButtonText>
                 <ButtonText>WhatsApp</ButtonText>
                 <ButtonText>Instagram</ButtonText>
-                <ButtonText>Linkedin</ButtonText>
             </div>
         </header>
     )
--- components/Title.jsx
+++ components/Title.jsx
@@ -4,9 +4,9 @@ import { ArrowLeftIcon } from '@radix-ui/react-icons';
 export default function Title(props) {
     return (
         <section className="flex flex-row content-center items-center justify-between mb-2">
-            <span className="flex flex-row gap-1 items-center font-normal text-sm group-hover:underline">{props.title}</span>
+            <span className="flex flex-row gap-1 items-center font-normal text-sm underline">{props.title}</span>
             <Link href="/">
-                <span className="flex flex-row gap-1 items-center font-normal text-sm group-hover:underline"><ArrowLeftIcon />Voltar</span>
+                <span className="flex flex-row gap-1 items-center font-normal text-sm hover:underline"><ArrowLeftIcon />Voltar</span>
             </Link>
         </section>
     );
